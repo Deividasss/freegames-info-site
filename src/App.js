21372671001369d428027581d2d657a467400304
Deividasss <kasetukas123@gmail.com>
2022-08-27 12:20:19 +0300

    update

--- src/App.js
+++ src/App.js
@@ -5,14 +5,18 @@ import GamesInfo from './components/GamesInfo/GamesInfo';
 import Navigation from './components/Nav/Navigation';
 import Footer from './components/Footer/Footer';
 import MainPage from './components/MainPage/MainPage';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    document.title = "NITRO free games"
+  }, []);
   return (
     <>
       <Router>
         <Navigation />
         <Routes>
-        <Route exact path='/' element={<MainPage />} />
+          <Route exact path='/' element={<MainPage />} />
           <Route exact path='/games' element={<Games />} />
           <Route exact path='/gameInfo' element={<GamesInfo />} />
         </Routes>
